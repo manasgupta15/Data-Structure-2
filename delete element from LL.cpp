ca687d86d4b6c74f58b63d6f6d5481564691cbc7
MANAS GUPTA <120665112+manasgupta15@users.noreply.github.com>
2024-03-09 11:26:38 +0530

    Create delete element from LL.cpp

---
+++ delete element from LL.cpp
@@ -0,0 +1,116 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int data1, Node* next1 = nullptr) {
+        data = data1;
+        next = next1;
+    }
+};
+
+Node* convertArrToLL(vector<int>& arr) {
+    if (arr.empty()) {
+        return nullptr; // Return nullptr for an empty vector
+    }
+
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+
+    for (int i = 1; i < arr.size(); i++) {
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+
+    return head; // Return the head of the linked list
+}
+
+void printLinkedList(Node* head) {
+    Node* temp = head;
+    while(temp) {
+        cout << temp->data << " ";
+        temp = temp -> next;
+    }
+}
+
+Node* deleteHead(Node* head) {
+    if(head == NULL) return head;
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+    return head;
+}
+
+Node* deleteTail(Node* head) {
+    if(head == NULL || head->next == NULL) return nullptr;
+    Node* temp = head;
+    while(temp->next->next != NULL) {
+        temp = temp->next;
+    }
+    
+    delete temp->next;
+    temp->next = NULL;
+    
+    return head;
+}
+
+Node* removeK(Node* head, int k) {
+    if(head == NULL) return head;
+    if(k == 1) {
+        Node* temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+    int cnt = 0;
+    Node* temp = head;
+    Node* prev = NULL;
+    while(temp!=NULL) {
+        cnt++;
+        if(cnt == k ) {
+            prev->next = prev->next->next;
+            free(temp);
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+
+Node* removeEl(Node* head, int el) {
+    if(head == NULL) return head;
+    if(head->data == el) {
+        Node* temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+    Node* temp = head;
+    Node* prev = NULL;
+    while(temp!=NULL) {
+        if(temp->data == el ) {
+            prev->next = prev->next->next;
+            free(temp);
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4};
+    Node* head = convertArrToLL(arr);
+
+    // printLinkedList(head);
+    
+    head = removeEl(head,3);
+    printLinkedList(head);
+    return 0;
+}
