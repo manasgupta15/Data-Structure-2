449ad8a69ac7b8f8581db00727b99e5b0fa5fcad
MANAS GUPTA <120665112+manasgupta15@users.noreply.github.com>
2024-03-17 22:36:22 +0530

    Create convert arr to doubly LL.cpp

---
+++ convert arr to doubly LL.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* back;
+    Node* next;
+
+    Node(int data1, Node* back1 = nullptr, Node* next1 = nullptr) {
+        data = data1;
+        back = back1;
+        next = next1;
+    }
+};
+
+Node* convertArrDD(vector<int>& arr) {
+    if(arr.empty())
+        return nullptr;
+    
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+    for(int i = 1 ; i < arr.size(); i++) {
+        Node* temp = new Node(arr[i], prev, nullptr);
+        prev -> next = temp;
+        prev = temp;
+    }
+    return head;
+}
+
+void print(Node* head) {
+    while(head != nullptr) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 5};
+    Node* head = convertArrDD(arr);
+    print(head);
+    return 0;
+}
